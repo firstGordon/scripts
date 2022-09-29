90ffea54ef54f5d5519e41361be6359c26fb752a
firstGordon <87316772+firstGordon@users.noreply.github.com>
2022-09-29 12:17:00 +0200

    Small chances to the console output and adding commentery

--- aToB/aToB.ps1
+++ aToB/aToB.ps1
@@ -1,21 +1,20 @@
+#######Viariables#########
 $file=$args[0]
 $fileB=$args[1]
 $wfile='tempA.csv'
 $wfileB='tempB.csv'
-$exportfile=$file
+#$exportfile=$file
+$exportfile='test.csv'
 $exportheader="Testdefinition;Bla;Bla"
-
 $header = 'Testdefinition', 'Bla1', 'Bla2'
 
-Get-Content $file | select -Skip 1 | Set-Content $wfile
-Get-Content $fileB | select -Skip 1 | Set-Content $wfileB
+#######Main#########
 
-#$search="Simone"
-#Write-Host $search
-#$linenumber= Get-Content $file | select-string $search
-#Write-Host $linenumber.LineNumber
+#remove header from inputfiles
+Get-Content $file | Select-Object -Skip 1 | Set-Content $wfile
+Get-Content $fileB | Select-Object -Skip 1 | Set-Content $wfileB
 
-
+#import as csv
 $csvA = Import-Csv $wfile -Delimiter ";" -Header $header
 $csvB = Import-Csv $wfileB -Delimiter ";" -Header $header
 
@@ -25,74 +24,53 @@ $csvB | Get-Member
 $csvA | Format-Table
 $csvB | Format-Table
 
-
+#begin to compare the files
 Write-Output ""
-Write-Output "Sucher"
-#   $csvB.Testdefinition | ForEach-Object {
-#    $testSearch = "$_"
-#    $csvA | Where-Object -Property Testdefinition -eq '$testSearch'
-#}
+Write-Output "Searching for failed results"
 
-#https://www.tutorialspoint.com/how-to-edit-the-csv-file-using-powershell
-
+#set header for exportfile
 Set-Content $exportfile -Value $exportheader
 
 $csvA.Testdefinition | ForEach-Object {
+    #get testnames
     Write-Output "The Testdefinition is: $_"
     $testSearch = $_
-    #Write-Output $testSearch
-    
+    #search testnames
     $testlinenumber = $csvB.Testdefinition | select-string $testSearch
     $testlinenumber = $testlinenumber.LineNumber
     $testlinecontent = $csvB.Testdefinition | select-string $testSearch
-    
+    #compare testnames
     if ($_ -eq $testlinecontent)
-    {        
+    {
         Write-Output "Testline found: $testlinecontent in Line $testlinenumber"
         Write-Output "$_ = $testlinecontent"
-
+        #build new row with "adhoc" qualifier
         $testlinexport = "$_;$testlinenumber;adhoc"
-                
-        Write-Output $testlinexport
-        $testlinexport | Add-Content -Path $exportfile       
-    }    
-    else
-    {    
-        Write-Output "Testline not found"
 
+        Write-Output $testlinexport
+        #add new row with "adhoc" qualifier to csv
+        $testlinexport | Add-Content -Path $exportfile
+    }
+    else
+    {
+        Write-Output "Testline not found"
+        #build new row with no qualifier
         $testlinexport = "$_;$testlinenumber;NULL"
 
         Write-Output $testlinexport
         $testlinexport | Add-Content -Path $exportfile
+        #add new row with no qualifier to csv
     }
-    
-    Write-Output $testlinexpor
-    #$testSearch.Bla2 = $testSearch.Bla2 -split ';\s*' + ('adhoc').Where({$testSearch.$_ -eq $testlinecontent} ) -join '; '
-    
-    #if ($_ -eq $testlinecontent)
-    #{
-        #$_.'Bla2' = "adhoc"        
-    #}
 
- 
     Write-Output ""
 }
 
+#read file back in fpr nice terminal output summary at the end
+$exportCsv = Import-Csv $exportfile -Delimiter ";" -Header $header
+$exportCsv | Format-Table
+
+#cleanup temp files
 Remove-Item -Path $wfile
 Remove-Item -Path $wfileB
-
-
-#$testlinexport | Export-Csv -Path test.csv -Delimiter ";" -NoTypeInformation
-#$testlinexport | Export-Csv -Path test.csv -Delimiter ";" -NoTypeInformation
-
-
-
-
-
-
-
-
-
-
 
 
